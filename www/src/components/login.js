b00e0479400f90fa48d3c279b2a2bc5bbb636d4f
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-03-23 17:27:23 -0700

    login form wip

--- www/src/components/login.js
+++ www/src/components/login.js
@@ -1,12 +1,35 @@
 //Home
 import React, { Component } from 'react';
 
-var Login = function(){
+class Login extends Component{
+  constructor(props);
+    super(props);
+    this.state = {
+      value = '';
+    }
+
+    handleChange(event){
+      this.setState( {value: event.target.value} );
+    }
+
+    handleLoginSubmit(event){
+
+    }
+  render(){
     return (
-        <div>
-            <h2>Login</h2>
-        </div>
+      <article>
+        <form onSubmit={(event)=this.handleLoginSubmit(event)}>
+            <input placeholder='Username'
+                   type='text'
+                   value={this.state.value}
+                   onChange={(event)=>this.handleChange(event)}></input>
+            <input type='submit'
+                   value='Send'></input>
+            <input>Submit</input>
+        </form>
+      </article>
     );
+  }
 }
 
 export default Login;
