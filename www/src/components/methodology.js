9161f5ef35d12cd683807227956c777f9d70c866
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-03-24 11:35:32 -0700

    methodology

--- www/src/components/methodology.js
+++ www/src/components/methodology.js
@@ -15,8 +15,12 @@ class Methods extends Component{
     render(){
         return (
             <Container>
-                <h1>The Reflected Best Self</h1>
-
+              <section className='wrapper' id='best-self'>
+                <h1>The Reflected Best-Self</h1>
+                <h2>Developed by Laura Morgan Roberts of Harvard Business School and Jane E. Dutton et. al of University of Michigan</h2>
+                <p>Reflected Best-Self seeks to improve the individuals self-potential through individual assessment and interpersonal feedback. </p>
+                <p className='quote'>"Being extraordinary does not necessarily mean obtaining a position of honor or glory or even of becoming successful in other people's eyes. It means being true to self. It means pursuing our full potential" -Quinn & Quinn, 2002.</p>
+              </section>
             </Container>
         );
      }
